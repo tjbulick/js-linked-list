85946b576954f7822f1c33cba97b913a18494570
tjbulick <e.pichtchalnikov@gmail.com>
2019-07-27 14:47:01 +0300

    add class node

---
+++ linked-list.js
@@ -0,0 +1,14 @@
+class Node {
+	constructor(value, next) {
+		this.value = value;
+		this.next = next;
+	}
+
+	getValue() {
+		return this.value
+	}
+
+	setValue(value) {
+		this.value = value
+	}
+}
