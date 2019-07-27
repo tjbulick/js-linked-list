7b428b7fd57280c25c64e707045c826e1b7d60ab
tjbulick <e.pichtchalnikov@gmail.com>
2019-07-27 16:21:18 +0300

    add printList function

--- linked-list.js
+++ linked-list.js
@@ -1,5 +1,5 @@
 class Node {
-	constructor(value, next) {
+	constructor(value, next = null) {
 		this.value = value;
 		this.next = next;
 	}
@@ -12,3 +12,18 @@ class Node {
 		this.value = value
 	}
 }
+
+let first = new Node(3);
+let middle = new Node(5);
+first.next = middle;
+let last = new Node(7);
+middle.next = last;
+
+const printList = (node) => {
+	while (node !== null) {
+		console.log(node.value, node.next);
+		node = node.next;
+	}
+}
+
+printList(first)
