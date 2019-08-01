797d1adfc4b999481f679e45255843e63e6f124f
tjbulick <e.pichtchalnikov@gmail.com>
2019-08-01 23:53:37 +0300

    compose main.js

--- linked-list.js
+++ linked-list.js
@@ -111,25 +111,3 @@ class LinkedList {
     	return false
 	}
 }
-
-const printList = (linkedList) => {
-	let node = linkedList.head;
-	while (node !== null) {
-		console.log(`data: ${node.value}, next: ${node.next}`);
-		node = node.next;
-	}
-}
-
-// tests
-
-let linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(2);
-linkedList.add(7);
-
-// for-of loop works fine!
-
-for (const data of linkedList) {
-	console.log(data)
-}
